cb1db9988bfeddf4b0dceb5f1750964e77fa19fd
atoz-arita <atoz.arita@gmail.com>
2026-05-07 10:41:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app.secret_key = "forest_secret_key"
 # --- Gmail送信設定 ---
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME", "atoz.arita@gmail.com")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME")
 
